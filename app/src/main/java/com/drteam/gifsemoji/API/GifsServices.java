3b54411016bb287fb5e3ed53a1e38ad479e66e5a
CUONG-PH <cuongph.uet14@gmail.com>
2017-04-12 21:50:34 +0700

    API implement

--- app/src/main/java/com/drteam/gifsemoji/API/GifsServices.java
+++ app/src/main/java/com/drteam/gifsemoji/API/GifsServices.java
@@ -1,5 +1,6 @@
 package com.drteam.gifsemoji.API;
 
+import com.drteam.gifsemoji.models.randomresult.RandomResult;
 import com.drteam.gifsemoji.models.searchdetail.SearchResult;
 import com.drteam.gifsemoji.models.searchdetail.SearchSingleResult;
 
@@ -16,17 +17,49 @@ import retrofit2.http.Query;
 
 public interface GifsServices {
 
+
+    /*
+    parameters:
+        q, s, tag : key word
+        api_key: API KEY trong Constants.java
+        limit: so ket qua image tra ra
+        offset: chua hieu no la cai gi nhung cu de la 0
+        rating: gom co: y,g, pg, pg-13 or r
+        lang: ngon ngu dinh dang 2 ky tu VD VietNam: vi
+        fmt: dinh dang response laf html hay json, mac dinh tra ra json
+     */
+
+    //Gifs
     //tìm kiếm dựa vào từ khóa
     @GET("v1/gifs/search")
-    Call<SearchResult> getSearchResult(@Query("q") String searchKey, @Query("api_key") String apikey, @Query("limit") int limit);
+    Call<SearchResult> getSearchByKeyResult(@Query("q") String searchKey,
+                                            @Query("api_key") String apikey,
+                                            @Query("limit") int limit,
+                                            @Query("offset") int offset,
+                                            @Query("rating") String rating,
+                                            @Query("lang") String lang,
+                                            @Query("fmt") String fmt);
 
     //ảnh gif trending
     @GET("v1/gifs/trending")
-    Call<SearchResult> getSearchTrendingResult(@Query("api_key") String apikey, @Query("limit") int limit);
+    Call<SearchResult> getSearchTrendingResult(@Query("api_key") String apikey,
+                                               @Query("limit") int limit,
+                                               @Query("rating") String rating,
+                                               @Query("fmt") String fmt);
 
     //ảnh gif liên quan
     @GET("v1/gifs/translate")
-    Call<SearchSingleResult> getSearchTranslateResult(@Query("s") String searchKey, @Query("api_key") String apikey);
+    Call<SearchSingleResult> getSearchTranslateResult(@Query("s") String searchKey,
+                                                      @Query("api_key") String apikey,
+                                                      @Query("rating") String rating,
+                                                      @Query("lang") String lang,
+                                                      @Query("fmt") String fmt);
+
+    @GET("v1/gifs/random")
+    Call<RandomResult> getSearchRandomResult(@Query("tag") String searchKey,
+                                                          @Query("api_key") String apikey,
+                                                          @Query("rating") String rating,
+                                                          @Query("fmt") String fmt);
 
     //tìm kiếm bởi 1 id
     @GET("v1/gifs/{gif_id}")
@@ -36,5 +69,37 @@ public interface GifsServices {
     //tìm kiếm bởi nhiều id
     @GET("v1/gifs")
     Call<SearchResult> getSearchByIdsResult(@Query("ids") List<String> gifIds, @Query("api_key") String apikey);
+
+
+
+    //STICKER - anhr gif khoong co background
+    @GET("v1/stickers/search")
+    Call<SearchResult> getSearchByKeyStickerResult(@Query("q") String searchKey,
+                                                   @Query("api_key") String apikey,
+                                                   @Query("limit") int limit,
+                                                   @Query("offset") int offset,
+                                                   @Query("rating") String rating,
+                                                   @Query("lang") String lang,
+                                                   @Query("fmt") String fmt);
+
+    @GET("v1/stickers/trending")
+    Call<SearchResult> getSearchTrendingStickerResult(@Query("api_key") String apikey,
+                                                      @Query("offset") int offset,
+                                                      @Query("limit") int limit,
+                                                      @Query("rating") String rating,
+                                                      @Query("fmt") String fmt);
+
+    @GET("v1/stickers/translate")
+    Call<SearchSingleResult> getSearchTranslateStickerResult(@Query("s") String searchKey,
+                                                      @Query("api_key") String apikey,
+                                                      @Query("rating") String rating,
+                                                      @Query("lang") String lang,
+                                                      @Query("fmt") String fmt);
+
+    @GET("v1/stickers/random")
+    Call<RandomResult> getSearchRandomStickerResult(@Query("tag") String searchKey,
+                                                    @Query("api_key") String apikey,
+                                                    @Query("rating") String rating,
+                                                    @Query("fmt") String fmt);
 }
 
